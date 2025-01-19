c648e93259d577e45637e149ebee364deb61c543
Aleksandr Filimonov <github@alexfilimon.dev>
2025-01-19 16:03:22 +0000

    Initial Commit

---
+++ DisneyStore/DisneyStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyStoreApp.swift
+//  DisneyStore
+//
+//  Created by Prequel on 19.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct DisneyStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
